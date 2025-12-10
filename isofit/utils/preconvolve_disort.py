abe2ce43dfedf81efbb33eb7dbffd7871200763d
brentwilder <brentonwilder1995@gmail.com>
2025-12-10 16:32:04 -0600

    update convolve util for disort

--- isofit/utils/preconvolve_disort.py
+++ isofit/utils/preconvolve_disort.py
@@ -2,7 +2,127 @@ import xarray as xr
 import numpy as np
 from joblib import Parallel, delayed
 
-from isofit.core.common import resample_spectrum
+
+
+def resample_spectrum(
+    x: np.array,
+    wl: np.array,
+    wl2: np.array,
+    fwhm2: np.array,
+    fill: bool = False,
+    srf_file: str = None,
+) -> np.array:
+    """Resample a spectrum to a new wavelength / FWHM.
+       Assumes Gaussian SRFs.
+
+    Args:
+        x: radiance vector
+        wl: sample starting wavelengths
+        wl2: wavelengths to resample to
+        fwhm2: full-width-half-max at resample resolution
+        fill: boolean indicating whether to fill in extrapolated regions
+        ### sc Adding for non-Gaussian SRF ###
+        srf_file: SRF for the sensor if not assuming Gaussian
+
+    Returns:
+        np.array: interpolated radiance vector
+
+    """
+    # sc including if else to add non-Gaussian SRF
+    # Probably a better way than this with file paths, etc.
+    if srf_file is None:
+        H = np.array(
+            [
+                spectral_response_function(wl, wi, fwhmi / 2.355)
+                for wi, fwhmi in zip(wl2, fwhm2)
+            ]
+        )
+        H[np.isnan(H)] = 0
+    else:
+        # Loading in user-supplied srf file and assigning variables
+        srf_data = xr.open_dataset(srf_file)
+        sensor_rsr, rsr_wls = srf_data.RSR.data, srf_data.wavelength.data
+
+        # Grabbing indices of rsr wavelengths which match sensor wavelengths (wl2)
+        idx = [np.argwhere(abs(rsr_wls - wl2[i]) < 0.1)[0] for i in range(len(wl2))]
+        idx = np.asarray(idx).flatten()
+
+        # Getting RSR at sensor spectral resolution
+        rsr_channel_res = sensor_rsr[:, idx]
+
+        # Normalize H to unit length
+        H = rsr_channel_res / np.sum(rsr_channel_res, axis=1)[:, np.newaxis]
+        H[np.isnan(H)] = 0
+
+    dims = len(x.shape)
+    if fill:
+        if dims > 1:
+            raise Exception("resample_spectrum(fill=True) only works with vectors")
+
+        x = x.reshape(-1, 1)
+        xnew = np.dot(H, x).ravel()
+        good = np.isfinite(xnew)
+        for i, xi in enumerate(xnew):
+            if not good[i]:
+                nearest_good_ind = np.argmin(abs(wl2[good] - wl2[i]))
+                xnew[i] = xnew[nearest_good_ind]
+        return xnew
+    else:
+        # Matrix
+        if dims > 1:
+            return np.dot(H, x.T).T
+
+        # Vector
+        else:
+            x = x.reshape(-1, 1)
+            return np.dot(H, x).ravel()
+
+
+def load_spectrum(spectrum_file: str) -> (np.array, np.array):
+    """Load a single spectrum from a text file with initial columns giving
+       wavelength and magnitude, respectively.
+
+    Args:
+        spectrum_file: file to load spectrum from
+
+    Returns:
+        np.array: spectrum values
+        np.array: wavelengths, if available in the file
+
+    """
+
+    spectrum = np.loadtxt(spectrum_file)
+    if spectrum.ndim > 1:
+        spectrum = spectrum[:, :2]
+        wavelengths, spectrum = spectrum.T
+        if wavelengths[0] < 100:
+            wavelengths = wavelengths * 1000.0  # convert microns -> nm if needed
+        return spectrum, wavelengths
+    else:
+        return spectrum, None
+
+
+def spectral_response_function(response_range: np.array, mu: float, sigma: float):
+    """Calculate the spectral response function.
+
+    Args:
+        response_range: signal range to calculate over
+        mu: mean signal value
+        sigma: signal variation
+
+    Returns:
+        np.array: spectral response function
+
+    """
+
+    u = (response_range - mu) / abs(sigma)
+    y = (1.0 / (np.sqrt(2.0 * np.pi) * abs(sigma))) * np.exp(-u * u / 2.0)
+    srf = y / y.sum()
+    return srf
+
+
+
+
 
 def load_sensor_spec(filepath):
     """
@@ -40,22 +160,24 @@ def resample_disort_lut(
 
         return np.stack(resampled).reshape(*shape, len(sensor_wl))
 
-    brdf_resampled = resample_variable(ds['brdf'].values)
-    hdrf_resampled = resample_variable(ds['hdrf'].values)
-    a_diff_resampled = resample_variable(ds['a_diff'].values)
+    brdf_resampled = resample_variable(ds['r_dd'].values)
+    hdrf_resampled = resample_variable(ds['r_hd'].values)
+    a_dir_resampled = resample_variable(ds['a_dd'].values)
+    a_diff_resampled = resample_variable(ds['a_hd'].values)
 
     # save to a new file
     ds_out = xr.Dataset(
         {
-            "brdf": (ds['brdf'].dims[:-1] + ('wavelength',), brdf_resampled),
-            "hdrf": (ds['hdrf'].dims[:-1] + ('wavelength',), hdrf_resampled),
-            "a_diff": (ds['a_diff'].dims[:-1] + ('wavelength',), a_diff_resampled)
+            "r_dd": (ds['r_dd'].dims[:-1] + ('wavelength',), brdf_resampled),
+            "r_hd": (ds['r_hd'].dims[:-1] + ('wavelength',), hdrf_resampled),
+            "a_dd": (ds['a_dd'].dims[:-1] + ('wavelength',), a_dir_resampled),
+            "a_hd": (ds['a_hd'].dims[:-1] + ('wavelength',), a_diff_resampled)
         },
         coords={**{k: ds.coords[k] for k in ds.coords if k != 'wavelength'},
                 "wavelength": sensor_wl}
     )
 
-    ds_out.attrs["description"] = "Resampled DISORT LUT for sensor"
+    ds_out.attrs["description"] = "Resampled DISORT LUT for EMIT sensor. Optically thick snow layer."
     ds_out.attrs["units"] = "wavelength in nanometers"
     ds_out.to_netcdf(output_nc_path)
 
@@ -65,9 +187,9 @@ def resample_disort_lut(
 if __name__ == "__main__":
     ########
     # Paths
-    input_nc = "/Users/bawilder/Code/snow/LUT/qaanaaq_ice_lut.nc"
-    sensor_file = "/Users/bawilder/Code/isofit-snow/pipeline/ang-wave.txt"
-    output_nc = "/Users/bawilder/Code/snow/LUT/qaanaaq_ice_lut_ANG.nc"
+    input_nc = "/Users/bawilder/Code/snow/LUT/disort_snow_lut.nc"
+    sensor_file = "/Users/bawilder/Code/melt-metrics/bw-wave/emit-wave.txt"
+    output_nc = "/Users/bawilder/Code/snow/LUT/disort_snow_lut_EMIT.nc"
     ########
 
 
