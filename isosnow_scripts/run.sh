86c638871886cf344c0b281ba16e522b78cf9929
brentwilder <brentonwilder1995@gmail.com>
2026-08-29 08:28:26 -0500

    Add logging to show which rho are being used

--- isosnow_scripts/run.sh
+++ isosnow_scripts/run.sh
@@ -21,7 +21,7 @@ EMULATOR_PATH="/Users/bawilder/Documents/sRTMnet/20251206_6c_5layer_-1.6c"
 ATMOS="ATM_MIDLAT_WINTER"
 
 # Path to surface config
-SURFACE_CONFIG_DIR="/Users/bawilder/Code/isofit-PRs/local/surfacelut.json"
+SURFACE_CONFIG_DIR="/Users/bawilder/Code/isofit-PRs/isosnow_scripts/surfacelut.json"
 
 # Output directory. Will be created if it doesn't exist.
 OUTPUT_DIR="/Users/bawilder/Code/isofit-PRs/local/test/20260828_snowmodel"
